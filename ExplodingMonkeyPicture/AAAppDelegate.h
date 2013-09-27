8b43f0672360f7be88dd1a8237172c5f356d8fa6
Kyle Oba <koba@pasdechocolat.com>
2013-09-27 11:24:22 -1000

    Initial Commit of project

---
+++ ExplodingMonkeyPicture/AAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AAAppDelegate.h
+//  ExplodingMonkeyPicture
+//
+//  Created by Kyle Oba on 9/27/13.
+//  Copyright (c) 2013 Kyle Oba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ExplodingMonkeyPicture/AAViewController.h
@@ -0,0 +1,13 @@
+//
+//  AAViewController.h
+//  ExplodingMonkeyPicture
+//
+//  Created by Kyle Oba on 9/27/13.
+//  Copyright (c) 2013 Kyle Oba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAViewController : UIViewController
+
+@end
